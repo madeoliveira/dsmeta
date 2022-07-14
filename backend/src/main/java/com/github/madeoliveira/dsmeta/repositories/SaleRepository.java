98a8f9de7323040ea6ce2cdcc4a2783a2675299c
Your Name <you@example.com>
2022-07-14 14:18:54 -0300

    API test

---
+++ backend/src/main/java/com/github/madeoliveira/dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.github.madeoliveira.dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.github.madeoliveira.dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale,Long >{
+
+}
---
+++ backend/src/main/java/com/github/madeoliveira/dsmeta/services/SaleService.java
@@ -0,0 +1,20 @@
+package com.github.madeoliveira.dsmeta.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.madeoliveira.dsmeta.entities.Sale;
+import com.github.madeoliveira.dsmeta.repositories.SaleRepository;
+
+@Service
+public class SaleService {
+
+	@Autowired
+	private SaleRepository repository;
+
+	public List<Sale> findSales() {
+		return repository.findAll();
+	}
+}
