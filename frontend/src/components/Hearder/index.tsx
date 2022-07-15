fa5a58a6473f445d3378032563be989a24b90ad6
Your Name <you@example.com>
2022-07-15 18:10:15 -0300

    Identification

--- frontend/src/components/Hearder/index.tsx
+++ frontend/src/components/Hearder/index.tsx
@@ -9,8 +9,9 @@ function Header() {
             <img src={logo} alt="DSMeta" />
             <h1>DSMeta</h1>
             <p>
-              Desenvolvido por
-              <a href="https://www.instagram.com/devsuperior.ig">@devsuperior.ig</a>
+            <a href='https://github.com/madeoliveira'>Marcio Alves de Oliveira </a>
+              <a href="https://www.instagram.com/oliveira.marcio_.ig"> @oliveira.marcio_</a>
+              
             </p>
         </div>
         </header>
